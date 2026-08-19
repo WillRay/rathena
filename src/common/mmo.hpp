8d2d39498a088fd8a333e4409ee17697bdeca033
William Ray <wray167@gmail.com>
2026-08-19 22:58:29 +1000

    verdant prontera

--- src/common/mmo.hpp
+++ src/common/mmo.hpp
@@ -42,15 +42,11 @@
 #define MAX_MAP_PER_SERVER 1500 /// Maximum amount of maps available on a server
 
 #ifndef INVENTORY_BASE_SIZE
-	#define INVENTORY_BASE_SIZE 150 // Amount of inventory slots each player has
+	#define INVENTORY_BASE_SIZE 200 // Amount of inventory slots each player has
 #endif
 
 #ifndef INVENTORY_EXPANSION_SIZE
-	#if PACKETVER_MAIN_NUM >= 20181031 || PACKETVER_RE_NUM >= 20181031 || PACKETVER_ZERO_NUM >= 20181114
-		#define INVENTORY_EXPANSION_SIZE 100 // Amount of additional inventory slots a player can have
-	#else
-		#define INVENTORY_EXPANSION_SIZE 0
-	#endif
+	#define INVENTORY_EXPANSION_SIZE 0
 #endif
 
 #ifndef MAX_INVENTORY
